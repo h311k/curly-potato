c9640a3e9ac2981cab3d5c957c65bcb137a63df9
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2020-02-25 13:38:26 -0300

    Revert "Autenticação e geração de JWT"
    This reverts commit d36ab29e24f4521d02194f9a09a97dbe882c4ab8.

--- routes/usuario.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/usuario.controller')
-
-router.post('/create', controller.create)
-router.post('/auth', controller.auth)
-
-module.exports = router
