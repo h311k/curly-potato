d36ab29e24f4521d02194f9a09a97dbe882c4ab8
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2020-02-25 13:31:52 -0300

    Autenticação e geração de JWT

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const mongoose = require('mongoose')
+const usuario = require('./routes/usuario.route')
+
+// Lendo o arquivo de configuracoes...
+const conf = JSON.parse(fs.readFileSync('app.conf'))
+
+// Armazena a chave de encriptacao do Crypto-JS
+chave = conf.chaveEncriptacaoSenhaUsuario
+
+// Armazena a chave do JWT
+chaveJWT = conf.chaveJWT
+
+// Estabelecendo conexao com o banco de dados
+mongoose.connect(conf.dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Erro na conexão com o MongoDB'))
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Associa a URI a rota
+app.use('/usuario', usuario)
+
+app.listen(80, () => {
+    console.log('Aplicação em execução.')
+})
