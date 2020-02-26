6e529638045408ba814228953a8376bb192aed14
Haony Duarte Porto Vieira <haonyduarte@gmail.com>
2020-02-26 14:32:17 -0300

    Adicionada função que retorna os dados de usuário da validação do JWT.

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -21,6 +21,21 @@ exports.auth = (req,res) => {
         'senha': security.encripta(req.body.senha),
     },'nome email dataCriacao',
     (err, usuario) => {
-        err? next(err) : usuario==null? res.send('Nome de usuário ou senha inválidos.') : res.send({auth: true, token: jwt.sign({usuario}, chaveJWT, {expiresIn: 300})})
+        err? next(err) : usuario==null? res.status(500).send('Nome de usuário ou senha inválidos.') : res.send({auth: true, token: jwt.sign({usuario}, chaveJWT, {expiresIn: 300})})
     })
+}
+
+let validaJWT = (req, res, next) => {
+    let token = req.headers['x-access-token']
+    if(!token) return res.status(401).send({ auth: false, message: 'No token provided.' })
+    jwt.verify(token, chaveJWT, (err, decoded) => {
+        if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' })
+        req.usuario = decoded // se o payload tiver mais elementos, como por exemplo perfil, associar aqui tambem.
+        next()
+    })
+}
+
+exports.getUsuario = (req, res, next) => {
+    validaJWT(req, res, next)
+    return req
 }
